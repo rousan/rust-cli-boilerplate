28b75fd7dae3a39e037fd6f64efd144763d01abe
Rousan Ali <rousanali786@gmail.com>
2020-04-10 23:53:57 +0530

    Add basic arguments

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ fn main() {
         .get_matches();
 
     let file_path = matches.value_of("file").unwrap();
-    let file_size = rust_cli_boilerplate::get_file_size(file_path).expect("File does not exist or can't access");
+    let file_size = match rust_cli_boilerplate::get_file_size(file_path) {
+        Ok(file_size) => file_size,
+        Err(_) => {
+            eprintln!("error: File does not exist or can't access");
+            return;
+        }
+    };
 
-    println!("File size: {} bytes", file_size);
+    println!("File size: {} B", file_size);
 }
